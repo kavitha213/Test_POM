ace51f2eefc7dcee579d3b4d72d6c6088472a8cc
kavitha213 <kavithavinoth121@gmail.com>
2023-03-29 14:25:29 +0530

    added new

---
+++ src/test/java/ObjectRepository/LoginPage.java
@@ -0,0 +1,56 @@
+package ObjectRepository;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Resources.BaseClass;
+
+public class LoginPage extends BaseClass {
+
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//a[text()='Sign In']")
+	private WebElement SignIn;
+	@FindBy(name = "username")
+	private WebElement username;
+	@FindBy(name = "password")
+	private WebElement password;
+	@FindBy(xpath = "//input[@value='Login']")
+	private WebElement login;
+
+	public WebElement getSignIn() {
+		return SignIn;
+	}
+
+	public void setSignIn(WebElement signIn) {
+		SignIn = signIn;
+	}
+
+	public WebElement getUsername() {
+		return username;
+	}
+
+	public void setUsername(WebElement username) {
+		this.username = username;
+	}
+
+	public WebElement getPassword() {
+		return password;
+	}
+
+	public void setPassword(WebElement password) {
+		this.password = password;
+	}
+
+	public WebElement getLogin() {
+		return login;
+	}
+
+	public void setLogin(WebElement login) {
+		this.login = login;
+	}
+
+}
